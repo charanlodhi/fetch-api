3449e069fe4e3cb2d3463e9e523f1a7800388a4c
Charanlodhi <charansingh.lodhi82@gmail.com>
2022-02-13 21:57:16 +0530

    add app all files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './List';
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Holidays List</h1>
+ <hr />
+ <List/>
+    </div>
+  );
+}
+
+export default App;
